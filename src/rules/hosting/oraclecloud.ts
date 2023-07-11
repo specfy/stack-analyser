259e7f50690124825ccc0c6fde5a1410f3ec0c9e
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-07-11 18:30:21 +0200

    fix(rules): oci has an npm package

--- src/rules/hosting/oraclecloud.ts
+++ src/rules/hosting/oraclecloud.ts
@@ -3,6 +3,7 @@ import { register } from '../../rules.js';
 register({
   tech: 'oraclecloud',
   dependencies: [
+    { type: 'npm', name: /^oci-/, example: 'oci-sdk' },
     { type: 'terraform', name: 'registry.terraform.io/oracle/oci' },
   ],
 });
