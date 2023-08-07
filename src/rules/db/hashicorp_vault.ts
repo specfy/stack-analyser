4f0ac2e82949bf214fb96a43424912e3046806ce
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-08-07 12:46:43 +0200

    fix: namespace Vault to avoid conflict

--- src/rules/db/hashicorp_vault.ts
+++ src/rules/db/hashicorp_vault.ts
@@ -1,7 +1,7 @@
 import { register } from '../../rules.js';
 
 register({
-  tech: 'vault',
+  tech: 'hashicorp_vault',
   dependencies: [
     { type: 'docker', name: /vault/, example: 'vault:0.0.0' },
     { type: 'terraform', name: 'registry.terraform.io/hashicorp/vault' },
