21a6b66f3463a791720af6195ad7af5466abf3bb
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-09-05 22:28:53 +0200

    fix(rules): add Tinybirdco

--- src/rules/analytics/index.ts
+++ src/rules/analytics/index.ts
@@ -3,4 +3,5 @@ import './googleanalytics.js';
 import './logsnag.js';
 import './mixpanel.js';
 import './plausible.js';
+import './tinybird.js';
 import './vercel.analytics.js';
--- src/types/techs.ts
+++ src/types/techs.ts
@@ -238,6 +238,7 @@ export type AllowedKeys =
   | 'tencentcloud'
   | 'terraform'
   | 'terragrunt'
+  | 'tinybird'
   | 'traefik'
   | 'travisci'
   | 'twilio'
