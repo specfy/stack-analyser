a8138097767f51210ea99711fce40ae6c13616ba
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-09-13 14:34:18 +0200

    fix(rules): add Jira

--- src/rules/saas/index.ts
+++ src/rules/saas/index.ts
@@ -24,6 +24,7 @@ import './hotjar.js';
 import './hubspot.js';
 import './huggingface.js';
 import './hypertune.js';
+import './jira.js';
 import './klaviyo.js';
 import './launchdarkly.js';
 import './logrocket.js';
---
+++ src/rules/saas/jira.ts
@@ -0,0 +1,21 @@
+import { register } from '../../register.js';
+
+register({
+  tech: 'jira',
+  name: 'Jira',
+  type: 'saas',
+  dependencies: [
+    { type: 'githubAction', name: 'atlassian/gajira-todo' },
+    { type: 'githubAction', name: 'github/ghas-jira-integration' },
+    { type: 'githubAction', name: 'atlassian/gajira-transition' },
+    { type: 'githubAction', name: 'atlassian/gajira-create' },
+    { type: 'githubAction', name: 'atlassian/gajira-cli' },
+    { type: 'githubAction', name: 'atlassian/gajira-find-issue-key' },
+    { type: 'githubAction', name: 'atlassian/gajira-comment' },
+    { type: 'docker', name: 'atlassian/jira-software' },
+    { type: 'npm', name: 'jira.js' },
+    { type: 'php', name: 'lesstif/php-jira-rest-client' },
+    { type: 'ruby', name: 'jira-ruby' },
+    { type: 'terraform.resource', name: 'gitlab_integration_jira' },
+  ],
+});
