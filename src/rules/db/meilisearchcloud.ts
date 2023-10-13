7c69754d44c0d26a4b083455ba32e804ccea890b
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-10-13 11:27:08 +0200

    fix(rules): add some cloud version

---
+++ src/rules/db/meilisearchcloud.ts
@@ -0,0 +1,7 @@
+import { register } from '../../register.js';
+
+register({
+  tech: 'meilisearchcloud',
+  name: 'Meilisearch Cloud',
+  type: 'db',
+});
---
+++ src/rules/db/typesensecloud.ts
@@ -0,0 +1,7 @@
+import { register } from '../../register.js';
+
+register({
+  tech: 'typesensecloud',
+  name: 'Typesense Cloud',
+  type: 'db',
+});
