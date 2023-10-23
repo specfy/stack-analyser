738c4b7a346db36a95d745f3cf63c47289152a5c
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-10-23 14:40:12 +0200

    fix(rules): add more docker for sentry

--- src/rules/monitoring/sentry.ts
+++ src/rules/monitoring/sentry.ts
@@ -5,6 +5,12 @@ register({
   name: 'Sentry',
   type: 'monitoring',
   dependencies: [
+    { type: 'docker', name: 'sentry' },
+    { type: 'docker', name: 'getsentry/sentry' },
+    { type: 'docker', name: 'getsentry/sentry-cli' },
+    { type: 'docker', name: 'sentryintegrations/sentry-github-action-release' },
+    { type: 'docker', name: 'daprio/sentry' },
+    { type: 'docker', name: 'us.gcr.io/sentryio/sentry' },
     { type: 'npm', name: /^@sentry\//, example: '@sentry/browser' },
     { type: 'rust', name: 'sentry' },
     { type: 'ruby', name: /^sentry-/, example: 'sentry-rails' },
