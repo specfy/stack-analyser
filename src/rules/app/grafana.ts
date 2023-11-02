d27ff11f9ca32cf6c5f6c516566274dfe78079be
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-11-02 11:18:48 +0100

    fix(rules): add more pkg for grafana

--- src/rules/app/grafana.ts
+++ src/rules/app/grafana.ts
@@ -6,6 +6,11 @@ register({
   type: 'app',
   dependencies: [
     { type: 'docker', name: 'grafana/grafana-enterprise' },
+    { type: 'docker', name: 'grafana/grafana-oss' },
+    { type: 'docker', name: 'grafana/grafana' },
+    { type: 'docker', name: 'grafana/agent' },
+    { type: 'docker', name: 'grafana/tempo' },
+    { type: 'docker', name: 'grafana/agent-operator' },
 
     {
       type: 'terraform',
