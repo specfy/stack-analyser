f9d86401990b547706970fd5b2124a230a2b954e
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-11-08 11:26:57 +0100

    fix(rules): add more pkg for wordpress

--- src/rules/php/wordpress.ts
+++ src/rules/php/wordpress.ts
@@ -10,5 +10,12 @@ register({
     { type: 'php', name: 'johnpbloch/wordpress' },
     { type: 'githubAction', name: '10up/action-wordpress-plugin-deploy' },
     { type: 'docker', name: 'wordpress' },
+    { type: 'npm', name: 'gatsby-source-wordpress' },
+    { type: 'npm', name: '@wordpress/block-editor' },
+    { type: 'npm', name: '@wordpress/data' },
+    { type: 'npm', name: '@wordpress/blocks' },
+    { type: 'npm', name: '@wordpress/api-fetch' },
+    { type: 'npm', name: '@wordpress/i18n' },
+    { type: 'npm', name: '@wordpress/hooks' },
   ],
 });
