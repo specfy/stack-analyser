c096c3952d4f5f83c8ca133747565c7285c88789
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-03-30 23:05:31 +0200

    fix(rules): add more packages for mixpanel

--- src/rules/analytics/mixpanel.ts
+++ src/rules/analytics/mixpanel.ts
@@ -4,5 +4,12 @@ register({
   tech: 'mixpanel',
   name: 'Mixpanel',
   type: 'analytics',
-  dependencies: [{ type: 'php', name: 'mixpanel/mixpanel-php' }],
+  dependencies: [
+    { type: 'php', name: 'mixpanel/mixpanel-php' },
+    { type: 'npm', name: 'mixpanel-browser' },
+    { type: 'python', name: 'mixpanel' },
+    { type: 'npm', name: 'mixpanel' },
+    { type: 'ruby', name: 'mixpanel-ruby' },
+    { type: 'golang', name: 'github.com/mixpanel/mixpanel-go' },
+  ],
 });
