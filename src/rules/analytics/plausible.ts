5153e3f6e1b188f6e1b3c83857c18bd00b2d0c3e
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-05-05 23:14:17 +0200

    fix: add more package for plausible

--- src/rules/analytics/plausible.ts
+++ src/rules/analytics/plausible.ts
@@ -4,5 +4,12 @@ register({
   tech: 'plausible',
   name: 'Plausible',
   type: 'analytics',
-  dependencies: [{ type: 'docker', name: 'plausible/analytics' }],
+  dependencies: [
+    { type: 'docker', name: 'plausible/analytics' },
+    { type: 'npm', name: 'plausible-tracker' },
+    { type: 'php', name: 'prestashop_plausible' },
+    { type: 'ruby', name: 'plausible_api' },
+    { type: 'php', name: 'vincentbean/laravel-plausible' },
+    { type: 'golang', name: 'github.com/andrerfcsantos/go-plausible/plausible' },
+  ],
 });
