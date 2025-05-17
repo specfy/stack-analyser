bf15ec064b00ab26a8b842aed8daec5abea95ac0
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-05-17 08:05:19 +0200

    feat(rules): add support for pyproject.toml

--- src/rules/spec/python/lockfile.test.ts
+++ src/rules/spec/python/lockfile.test.ts
@@ -7,7 +7,7 @@ import { FakeProvider } from '../../../provider/fake.js';
 
 import '../../../autoload.js';
 
-describe('python (lockfile)', () => {
+describe('python lockfile: requirements.txt', () => {
   it('should match everything', async () => {
     const deps = [];
     for (const item of rawList) {
@@ -53,6 +53,53 @@ describe('python (lockfile)', () => {
             // gte-lt
             'datadog>=1.4.3,<7.0.0',
           ].join('\n'),
+        },
+      }),
+    });
+
+    const merged = flatten(res, { merge: true });
+
+    expect([...merged.techs].sort()).toMatchSnapshot();
+    expect([...merged.dependencies].sort()).toMatchSnapshot();
+  });
+});
+
+describe('python lockfile: pyproject.toml', () => {
+  it('should match everything', async () => {
+    const deps = [];
+    for (const item of rawList) {
+      if (item.type !== 'dependency' || item.ref.type !== 'python') {
+        continue;
+      }
+
+      const example = 'example' in item.ref ? item.ref.example : item.ref.name;
+      deps.push(`"${example}==1.0.0"`);
+    }
+
+    const lockfile = `[project]
+name = "test"
+description = "This is a test"
+
+dependencies = [
+${deps.join(',\r\n')}
+]
+
+[project.optional-dependencies]
+memory = [
+    "sentence-transformers>=4.0.2",
+]
+
+[tool.ruff]
+line-length = 130
+`;
+
+    const res = await analyser({
+      provider: new FakeProvider({
+        paths: {
+          '/': ['pyproject.toml'],
+        },
+        files: {
+          '/pyproject.toml': lockfile,
         },
       }),
     });
--- src/rules/spec/python/lockfile.ts
+++ src/rules/spec/python/lockfile.ts
@@ -1,62 +1,144 @@
 import path from 'node:path';
 
+import toml from '@iarna/toml';
+
+import { l } from '../../../common/log.js';
 import { matchDependencies } from '../../../matchDependencies.js';
 import { Payload } from '../../../payload/index.js';
 
+import type { ProviderFile } from '../../../provider/base.js';
 import type { Dependency } from '../../../types/index.js';
 import type { ComponentMatcher } from '../../../types/rule.js';
 
 const LOCKFILE = 'requirements.txt';
+const PROJECT = 'pyproject.toml';
 const lineReg = /(^([a-zA-Z0-9._-]+)$|^([a-zA-Z0-9._-]+)(([>=]+)([0-9.]+)))/;
+
+interface Pyproject {
+  project?: {
+    dependencies?: string[];
+  };
+}
 
 export const detectPythonLockfile: ComponentMatcher = async (files, provider) => {
   for (const file of files) {
-    if (file.name !== LOCKFILE) {
+    if (file.name === PROJECT) {
+      const content = await provider.open(file.fp);
+      if (!content) {
+        continue;
+      }
+
+      const pl = new Payload({
+        name: 'virtual',
+        folderPath: path.dirname(file.fp),
+      });
+      const res = parsePyproject({ content, pl, file });
+      if (!res) {
+        continue;
+      }
+
+      return pl;
+    } else if (file.name === LOCKFILE) {
+      const content = await provider.open(file.fp);
+      if (!content) {
+        continue;
+      }
+
+      const pl = new Payload({
+        name: 'virtual',
+        folderPath: path.dirname(file.fp),
+      });
+      parseRequirementsTxt(content, pl);
+
+      return pl;
+    } else {
       continue;
     }
-
-    const content = await provider.open(file.fp);
-    if (!content) {
-      continue;
-    }
-
-    const pl = new Payload({
-      name: 'virtual',
-      folderPath: path.dirname(file.fp),
-    });
-
-    const dependencies: Dependency[] = [];
-    // We only register docker service with image and that we know
-    for (const line of content.split(/\r?\n/)) {
-      if (line.startsWith('#')) {
-        continue;
-      }
-
-      const match = line.match(lineReg);
-      if (!match) {
-        continue;
-      }
-
-      const name = match[2] || match[3];
-      const version = match[6] || 'latest';
-
-      if (!name) {
-        continue;
-      }
-
-      dependencies.push(['python', name, version || 'latest']);
-      const matched = matchDependencies([name], 'python');
-      if (matched.size <= 0) {
-        continue;
-      }
-
-      pl.addTechs(matched);
-    }
-
-    pl.dependencies = [...dependencies];
-
-    return pl;
   }
 
   return false;
 };
+
+export function parseRequirementsTxt(content: string, pl: Payload): void {
+  const dependencies: Dependency[] = [];
+
+  for (const line of content.split(/\r?\n/)) {
+    if (line.startsWith('#')) {
+      continue;
+    }
+
+    const match = line.match(lineReg);
+    if (!match) {
+      continue;
+    }
+
+    const name = match[2] || match[3];
+    const version = match[6] || 'latest';
+
+    if (!name) {
+      continue;
+    }
+
+    dependencies.push(['python', name, version || 'latest']);
+    const matched = matchDependencies([name], 'python');
+    if (matched.size <= 0) {
+      continue;
+    }
+
+    pl.addTechs(matched);
+  }
+
+  pl.dependencies = [...dependencies];
+}
+
+export function parsePyproject({
+  content,
+  pl,
+  file,
+}: {
+  content: string;
+  pl: Payload;
+  file: ProviderFile;
+}): boolean {
+  let json: Pyproject;
+  try {
+    json = toml.parse(content) as unknown as Pyproject;
+  } catch (err) {
+    l.warn('Failed to parse Cargo.toml', file.fp, err);
+    return false;
+  }
+
+  if (
+    !('project' in json) ||
+    !json.project ||
+    !('dependencies' in json.project) ||
+    !json.project.dependencies
+  ) {
+    return false;
+  }
+
+  const dependencies: Dependency[] = [];
+  for (const dep of json.project.dependencies) {
+    const match = dep.match(lineReg);
+    if (!match) {
+      continue;
+    }
+
+    const name = match[2] || match[3];
+    const version = match[6] || 'latest';
+    if (!name) {
+      continue;
+    }
+
+    dependencies.push(['python', name, version || 'latest']);
+    const matched = matchDependencies([name], 'python');
+    if (matched.size <= 0) {
+      continue;
+    }
+
+    pl.addTechs(matched);
+  }
+
+  pl.dependencies = [...dependencies];
+  return true;
+}
